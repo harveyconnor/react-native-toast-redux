4ee79a22ae8a61542d449fdae7cd7b0e7e0affa9
Harvey Connor <harveyconnor@SG-MacBook-Pro.pixeltech.priv>
2018-12-12 12:38:51 +1100

    Documentation 1.

--- src/toast/index.ts
+++ src/toast/index.ts
@@ -4,4 +4,15 @@ import Toast from './Toast';
 
 const mapStateToProps = (store: any) => store.toast;
 
+/**
+ * Use component to display toast messages
+ *
+ * ### Example
+ *
+ * ```
+ * <Provider store={store}>     
+ *   <Toast />
+ * </Provider>
+ * ```
+ */
 export default connect(mapStateToProps)(Toast);
